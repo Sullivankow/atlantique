bc774da75ac90a43721c8fa695b2c1ba9b4b52e2
SunDev <sundly@live.fr>
2025-08-08 18:29:08 +0200

    Update reviews section

--- send_message.php
+++ send_message.php
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Contact - Les clés de l'Atlantique</title>
-    <!-- <meta http-equiv="refresh" content="4;url=index.php#contact"> -->
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
--- template/reviews.php
+++ template/reviews.php
@@ -23,7 +23,7 @@
                             <circle cx="40" cy="40" r="35" fill="#4ce0d2" />
                         </svg>
                     </div>
-                    <div class="d-grid" style="position:relative; z-index:3; width:fit-content;">
+                    <div class="d-grid mt-3" style="position:relative; z-index:3; width:fit-content; ">
                         <a href="pages/biens.php#bien" class="btn btn-primary mt-auto animate-btn rounded-pill">Voir nos biens</a>
                     </div>
                 </div>
