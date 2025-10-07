f9de204db90e38435059ae9caa620d9fa21ae95d
SunDev <sundly@live.fr>
2025-10-07 15:58:45 +0200

    update css

--- template/header.php
+++ template/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="/atlantique/assets/css/override-bootstrap.css">
+    <link rel="stylesheet" href="/assets/css/override-bootstrap.css">
     <title>Les Clés de l’Atlantique – Conciergerie à Royan & Côte Atlantique</title>
 </head>
 
